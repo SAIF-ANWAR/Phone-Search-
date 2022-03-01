e2e28bf671ba2494c2883ae245a50dcf05892e3a
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-03-01 14:49:58 +0600

    phone-hunter

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const searchPhones = () => {
     const searchInput = document.getElementById('search-input');
     const searchText = searchInput.value;
     searchInput.value = "";
-    // console.log(searchText);
+
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
         .then(res => res.json())
         .then(data => displayPhones(data.data));
@@ -18,14 +18,19 @@ const displayPhones = data => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
+       
+        <div class="card h-100"> 
+ 
         <img width="50px" height="350px" src="${phone.image}"  class="card-img-top">
+    
             <div class="card-body">
             <h2 class="card-title">Phone Name: ${phone.phone_name} </h2>
             <h4 class="card-title"> Brand: ${phone.brand} </h4>
             <button onclick="showDetails('${phone.slug}')" > Details</button>
             </div>
         </div>
+
+       
         `;
         searchResult.appendChild(div);
     }
@@ -42,25 +47,29 @@ const showDetails = slug => {
 }
 
 const showDetailsOnWeb = phone => {
+    console.log(phone)
     const displayDetails = document.getElementById('display-details');
     const div = document.createElement('div');
     div.innerHTML = `
     <div class="card " style="width: 18rem;">
     <img src="${phone.image}" class="card-img-top" alt="...">
     <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
+        <h5 class="card-title">"${phone.name} "</h5>
+        <p id="release-date">Release Date: ${phone.releaseDate}</p>
+        
     </div>
     <ul class="list-group slist-group-flush">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-    </ul>
+        <li class="list-group-item">Chipset: ${phone.mainFeatures.chipSet}</li>
+        <li class="list-group-item">Display: ${phone.mainFeatures.displaySize}</li>
+        <li class="list-group-item">Memory: ${phone.mainFeatures.memory}</li>
 
+    </ul>
         </div>
-    
     `
     displayDetails.appendChild(div);
+    if (phone.releaseDate = ' ') {
+        const p = document.getElementById('release-date');
+        p.innerText = "Release date is not available";
+    }
 
 }
