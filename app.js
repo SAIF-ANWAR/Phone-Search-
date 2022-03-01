145ec6faedcd7d4a050b97b2676858623d4e9740
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-03-01 11:54:14 +0600

    phone-hunter

--- app.js
+++ app.js
@@ -14,19 +14,24 @@ searchPhones()
 const displayPhones = data => {
     const searchResult = document.getElementById('search-result');
     for (const phone of data) {
-        console.log(phone)
+        // console.log(phone)
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-        <img src="${phone.image}"  class="card-img-top" alt="...">
-        <div class="card-body">
-            <h2 class="card-title">${phone.phone_name} </h2>
-            <h5 class="card-title">${phone.brand} </h5>
-            <button> Details</button>
+        <img width="50px" height="250px" src="${phone.image}"  class="card-img-top">
+            <div class="card-body">
+            <h2 class="card-title">Phone Name: ${phone.phone_name} </h2>
+            <h5 class="card-title"> Brand: ${phone.brand} </h5>
+            <button onclick="showDetails('${phone.slug}')" > Details</button>
+            </div>
         </div>
         `;
         searchResult.appendChild(div);
     }
 
+}
+
+const showDetails = slug => {
+    // console.log(slug);
 }
