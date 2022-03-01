4e93ca774db8ea04fc2419e1ea31411f62fe121b
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-03-01 11:18:03 +0600

    phone-hunter

--- app.js
+++ app.js
@@ -12,9 +12,21 @@ const searchPhones = () => {
 searchPhones()
 
 const displayPhones = data => {
-
+    const searchResult = document.getElementById('search-result');
     for (const phone of data) {
         console.log(phone)
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+        <img src="${phone.image}"  class="card-img-top" alt="...">
+        <div class="card-body">
+            <h2 class="card-title">${phone.phone_name} </h2>
+            <h5 class="card-title">${phone.brand} </h5>
+            <button> Details</button>
+        </div>
+        `;
+        searchResult.appendChild(div);
     }
 
 }
