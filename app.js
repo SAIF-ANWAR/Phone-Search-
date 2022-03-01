2100321872fea9ac210ceb4763acf0121bb28061
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-03-01 12:31:39 +0600

    phone-hunter

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ const displayPhones = data => {
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-        <img width="50px" height="250px" src="${phone.image}"  class="card-img-top">
+        <img width="50px" height="350px" src="${phone.image}"  class="card-img-top">
             <div class="card-body">
             <h2 class="card-title">Phone Name: ${phone.phone_name} </h2>
-            <h5 class="card-title"> Brand: ${phone.brand} </h5>
+            <h4 class="card-title"> Brand: ${phone.brand} </h4>
             <button onclick="showDetails('${phone.slug}')" > Details</button>
             </div>
         </div>
@@ -33,5 +33,34 @@ const displayPhones = data => {
 }
 
 const showDetails = slug => {
-    // console.log(slug);
+    console.log(slug);
+    const url = (`https://openapi.programming-hero.com/api/phone/${slug}`);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showDetailsOnWeb(data.data))
+
+}
+
+const showDetailsOnWeb = phone => {
+    const displayDetails = document.getElementById('display-details');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="card " style="width: 18rem;">
+    <img src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+    </div>
+    <ul class="list-group slist-group-flush">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+    </ul>
+
+        </div>
+    
+    `
+    displayDetails.appendChild(div);
+
 }
